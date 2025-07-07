05e29f6008759e4f26c1ef98380d6ca3b784c907
kamilyroch <144927094+kamilyroch@users.noreply.github.com>
2025-07-07 08:56:45 -0300

    aula 08*

--- main.js
+++ main.js
@@ -58,8 +58,6 @@ function geraSenha() {
 }
 
 function classificaSenha(tamanhoAlfabeto) {
-    const valorEntropia = document.querySelector(`.entropia`)
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     let entropia = tamanhoSenha*Math.log2(tamanhoAlfabeto)
     console.log(entropia);
@@ -71,4 +69,6 @@ function classificaSenha(tamanhoAlfabeto) {
     } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
     }
+    const valorEntropia = document.querySelector(`.entropia`)
+    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
